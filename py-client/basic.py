aa562fbab84d4f9ff7a091eb07ec80779c807c15
FiahIdris <nurfiahidris098@gmail.com>
2022-04-08 12:57:37 +0800

    serializer validation

--- py-client/basic.py
+++ py-client/basic.py
@@ -3,6 +3,6 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint,params={"abc":123 },json={"query":"Hello World"})
+get_response = requests.post(endpoint,params={"abc":123 },json={"content_type":"Hello World"})
 # print(get_response.te 
 print(get_response.json())
