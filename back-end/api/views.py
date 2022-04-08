799c2006f35b71f93bb3b5aac9853e82abbbffa3
FiahIdris <nurfiahidris098@gmail.com>
2022-04-08 10:03:25 +0800

    playing-with-views

--- back-end/api/views.py
+++ back-end/api/views.py
@@ -1,6 +1,17 @@
 from django.http import JsonResponse
+import json
 
 def api_home(request,*args,**kwargs):
-    return JsonResponse({
-        "message": "Hi there, this is your django  API response"
-    })
+    body = request.body #byte string of json data
+    
+   
+    data ={}
+    try:
+        data = json.loads(body)  # json to python dictionary
+    except:
+        pass
+    # print(data)
+    data['params'] = dict(request.GET) # url query params
+    data['headers'] =dict(request.headers)
+    data['content-type'] = request.content_type
+    return JsonResponse(data)
--- py-client/basic.py
+++ py-client/basic.py
@@ -3,7 +3,6 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint,json={"query":"Hello World"})
-print(get_response.text)
-print(get_response.status_code)
+get_response = requests.get(endpoint,params={"abc":123 },json={"query":"Hello World"})
+# print(get_response.te 
 print(get_response.json())
