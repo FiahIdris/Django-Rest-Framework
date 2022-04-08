3e76ac7bcca7253b34c4d7eb15c479a883b0fb98
FiahIdris <nurfiahidris098@gmail.com>
2022-04-08 09:42:09 +0800

    initial-commit

---
+++ py-client/basic.py
@@ -0,0 +1,9 @@
+import json
+import requests
+
+endpoint = "http://localhost:8000/api/"
+
+get_response = requests.get(endpoint,json={"query":"Hello World"})
+print(get_response.text)
+print(get_response.status_code)
+print(get_response.json())
